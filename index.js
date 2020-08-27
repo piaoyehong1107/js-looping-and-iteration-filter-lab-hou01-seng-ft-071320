0b014314c8fac1e1d1b9cca8e10acad53d3f2c5b
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-27 04:45:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file
+function findMatching(drivers,string){
+  return drivers.filter(function(driver){return driver.toLowerCase()===string.toLowerCase();});
+}
+function fuzzyMatch(drivers,string){
+  return drivers.filter(function(driver){return driver.indexOf(string)===0;});
+}
+function matchName(drivers,string){
+  return drivers.filter(function(driver){return driver.name===string;});
+}
